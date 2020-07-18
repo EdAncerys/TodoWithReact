98df4d8e36a54e62864dd36fce1a1c605a8f7f60
Ed <lookatemail@gmail.com>
2020-07-18 13:43:05 +0100

    Adding props and passing to todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todos) => <h3>{todos.title}</h3>);
+  }
 }
 
 export default Todos;
