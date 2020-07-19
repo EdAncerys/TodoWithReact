f2a64804bb29df6399b3172542da8fb477b050d3
Ed <lookatemail@gmail.com>
2020-07-19 16:18:15 +0100

    Add about route with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+import About from './components/pages/About';
 import { v4 as uuidv4 } from 'uuid';
 
 import './css/App.css';
@@ -60,17 +62,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="container">
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
