079d424f03950e159f983d39044d81a19574d541
Ed <lookatemail@gmail.com>
2020-07-19 15:16:00 +0100

    Add header styling

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header style={headerStyle} />
+        <Header />
         <Todos
           todos={this.state.todos}
           markComplete={this.markComplete}
